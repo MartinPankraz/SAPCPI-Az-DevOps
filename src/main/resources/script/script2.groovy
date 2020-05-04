e3f6f96ca271d308e571bf72aab99a3fc908d505
Martin Pankraz <mapankra@microsoft.com>
2020-05-04 15:24:38 +0100

    initial commit

---
+++ src/main/resources/script/script2.groovy
@@ -0,0 +1,55 @@
+/*
+ The integration developer needs to create the method processData 
+ This method takes Message object of package com.sap.gateway.ip.core.customdev.util 
+which includes helper methods useful for the content developer:
+The methods available are:
+    public java.lang.Object getBody()
+	public void setBody(java.lang.Object exchangeBody)
+    public java.util.Map<java.lang.String,java.lang.Object> getHeaders()
+    public void setHeaders(java.util.Map<java.lang.String,java.lang.Object> exchangeHeaders)
+    public void setHeader(java.lang.String name, java.lang.Object value)
+    public java.util.Map<java.lang.String,java.lang.Object> getProperties()
+    public void setProperties(java.util.Map<java.lang.String,java.lang.Object> exchangeProperties) 
+    public void setProperty(java.lang.String name, java.lang.Object value)
+    public java.util.List<com.sap.gateway.ip.core.customdev.util.SoapHeader> getSoapHeaders()
+    public void setSoapHeaders(java.util.List<com.sap.gateway.ip.core.customdev.util.SoapHeader> soapHeaders) 
+       public void clearSoapHeaders()
+ */
+import com.sap.gateway.ip.core.customdev.util.Message;
+import java.util.HashMap;
+import groovy.json.JsonBuilder;
+
+def Message processData(Message message) {
+    //Body
+    //def body = message.getBody();
+    //Get Headers 
+    def headersMap = message.getHeaders();
+    message.setHeader("Content-Type","application/json");
+    def statusCode = headersMap.get("CamelHttpResponseCode");
+	//Get Properties  
+	def map = message.getProperties();
+	def mytime = map.get("CamelCreatedTimestamp");
+	def name = map.get("iflowName");
+	def myErrorMsg = map.get("CamelExceptionCaught");
+    def appUrl = map.get("tenantUrl");
+	def mylogId = map.get("SAP_MessageProcessingLogID");
+	//def runId = idValue;
+	def targetUrl = appUrl + "%7B%22messageGuid%22%3A%22" + mylogId + "%22%7D";
+	//def targetUrlDetails = appUrl + 'MessageProcessingRun/%7B"parentContext":%7B"MessageMonitor":%7B"artifact":"CreateIssueInAzureDevOps","artifactKey":"CreateIssueInAzureDevOps","artifactName":"CreateIssueInAzureDevOps"%7D%7D,"messageProcessingLog":"' + mylogId + '","RunId":"' + runId + '"%7D';
+	
+	def builder = new JsonBuilder();
+    builder.obj {
+        integrationPackage 'AzureIntegrationExample'
+        iflowName name
+        httpCode statusCode
+        time mytime
+        url targetUrl
+        logid mylogId 
+    //    urlDetail targetUrlDetails
+        msg myErrorMsg.getMessage()
+    }
+     
+    message.setBody(builder.toPrettyString());
+       
+       return message;
+}
---
+++ src/main/resources/script/script3.groovy
@@ -0,0 +1,49 @@
+/*
+ The integration developer needs to create the method processData 
+ This method takes Message object of package com.sap.gateway.ip.core.customdev.util 
+which includes helper methods useful for the content developer:
+The methods available are:
+    public java.lang.Object getBody()
+	public void setBody(java.lang.Object exchangeBody)
+    public java.util.Map<java.lang.String,java.lang.Object> getHeaders()
+    public void setHeaders(java.util.Map<java.lang.String,java.lang.Object> exchangeHeaders)
+    public void setHeader(java.lang.String name, java.lang.Object value)
+    public java.util.Map<java.lang.String,java.lang.Object> getProperties()
+    public void setProperties(java.util.Map<java.lang.String,java.lang.Object> exchangeProperties) 
+    public void setProperty(java.lang.String name, java.lang.Object value)
+    public java.util.List<com.sap.gateway.ip.core.customdev.util.SoapHeader> getSoapHeaders()
+    public void setSoapHeaders(java.util.List<com.sap.gateway.ip.core.customdev.util.SoapHeader> soapHeaders) 
+       public void clearSoapHeaders()
+ */
+import com.sap.gateway.ip.core.customdev.util.Message;
+import java.util.HashMap;
+import java.text.SimpleDateFormat;
+import java.io.*
+import java.lang.*;
+import java.util.*;
+
+def Message processData(Message message) {
+
+       def map = message.getProperties();
+       def alreadyExists = map.get("alreadyExists");
+       //default already exists to past by default
+       if(alreadyExists == ""){
+           alreadyExists = new Date(System.currentTimeMillis()-24*60*60*1000).format( "yyyy-MM-dd hh:mm" );
+       }
+       SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd hh:mm");
+       Date currentDate = sdf.parse(new Date().format( "yyyy-MM-dd hh:mm" ));
+       Date storedDate = sdf.parse(alreadyExists); 
+       
+       message.setProperty("printDebugCurrent", currentDate);
+       message.setProperty("printDebug", storedDate);
+       def dateDiff = currentDate - storedDate;
+       message.setProperty("printDebugDiff", dateDiff);
+       
+       if(dateDiff == 1){
+           message.setProperty("skipAzure", "false");    
+       }else{
+           message.setProperty("skipAzure", "true");
+       }
+       
+       return message;
+}
