f13abae51b5cb1339f3f9747eb0f3bbe7a0061f6
Martin Pankraz <mapankra@microsoft.com>
2020-06-05 19:12:00 +0100

    check code coverage

--- iFlows/CreateIssueInAzureDevOps/src/main/resources/script/script1.groovy
+++ iFlows/CreateIssueInAzureDevOps/src/main/resources/script/script1.groovy
@@ -18,13 +18,23 @@ The methods available are:
 import com.sap.gateway.ip.core.customdev.util.Message;
 import java.util.HashMap;
 def Message processData(Message message) {
-    //Body 
-       //def body = message.getBody();
-       //Headers 
-       def map = message.getHeaders();
-       def value = map.get("CamelHttpResponseCode");
-       //Properties 
-       map = message.getProperties();
-       message.setProperty("returnCode", value);
+	
+	def map = message.getHeaders();
+	String httpQuery = map.get("CamelHttpQuery");
+	def value = map.get("CamelHttpResponseCode");
+	//Properties
+	message.setProperty("returnCode", value);
+	// Split parameters by &
+	if(httpQuery) {
+		String[] parameters = httpQuery.split("&")
+		parameters.each {
+			int index = it.indexOf("=")
+			def decodeQuery = { input ->
+				URI uri = new URI("http://localhost?" + input)
+				return uri.getQuery()
+			}
+			message.setProperty(it.substring(0, index), decodeQuery(it.substring(index + 1)))
+		} 
+	}
        return message;
 }
